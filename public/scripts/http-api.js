b422d165a9cee2cecc921f666138f4a5ba273627
Александр <nikol.alex06@mail.ru>
2026-03-18 08:41:07 +0300

    feat: add remote host for api

--- public/scripts/http-api.js
+++ public/scripts/http-api.js
@@ -2,7 +2,7 @@ class HttpRequest {
   #api_host;
 
   constructor() {
-    this.#api_host = 'http://localhost:3000/api';
+    this.#api_host = 'https://eventhubnest.onrender.com/api';
   }
 
   async get({ onError, onSuccess, query }) {
