3775b68ffa9a397414c5f3d6bb6e5cba108129b9
Александр <nikol.alex06@mail.ru>
2026-04-13 23:48:55 +0300

    fix: remove profile

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersApiController } from './users.api.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UsersController } from './users.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UsersApiController, UsersController],
+  controllers: [UsersApiController],
   providers: [UsersService],
   exports: [UsersService],
 })
