4dd594ad6df6d8bb253f9561daf8b3dd216f628a
Александр <nikol.alex06@mail.ru>
2026-04-15 08:50:13 +0300

    fix

--- public/scripts/http-api.js
+++ public/scripts/http-api.js
@@ -2,7 +2,7 @@ class HttpRequest {
   #api_host;
 
   constructor() {
-    this.#api_host = 'https://eventhubnest.onrender.com/api';
+    this.#api_host = '/api';
   }
 
   async #request({ method, query, body, onSuccess, onError }) {
